6d1fc57a71d7c8d69818acc6b3b67eeaeb565a26
elvintern <elvintern@gmail.com>
2022-12-23 18:27:40 +1300

    Add smtpJS function

--- resources/js/index.js
+++ resources/js/index.js
@@ -12,3 +12,25 @@ document.addEventListener(
   },
   { passive: true }
 );
+
+function sendEmail() {
+  Email.send({
+    Host: 'smtp.elasticemail.com',
+    Username: 'elvintern@gmail.com',
+    Password: '6EDBE6B03E7FEA88BE99F13FB2E19486BF9C',
+    To: 'elvintern@gmail.com',
+    From: 'elvintern@gmail.com',
+    Subject: 'New Contact From Portfolio Page',
+    Body:
+      'Name: ' +
+      document.getElementById('name').value +
+      '<br> Email: ' +
+      document.getElementById('email').value +
+      '<br> Message: ' +
+      document.getElementById('message').value,
+  }).then(() =>
+    alert(
+      'Thank you for getting in touch! I will get back in touch with you soon! Have a nice day :)'
+    )
+  );
+}
