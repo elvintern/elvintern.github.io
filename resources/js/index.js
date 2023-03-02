8d69516126e4f7f54f22e99880e2bdb4d7a6ebd9
elvintern <elvintern@gmail.com>
2023-03-02 14:48:56 +1300

    Add a validation check for name input in contact form

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,6 +4,7 @@ const hamburger = document.querySelector('.hamburger');
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 const navMenu = document.querySelector('.nav__list--menu');
 const alertMsg = document.querySelector('.alert-msg');
+const form = document.querySelector('form');
 
 document.addEventListener(
   'scroll',
@@ -22,6 +23,26 @@ hamburger.addEventListener('click', () => {
   hamburgerIcon.classList.toggle('fa-times');
 });
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let name = document.getElementById('name');
+  if (name.value.trim().length < 2) {
+    alertMsg.classList.remove('alert-msg--success');
+    alertMsg.classList.add('alert-msg--error');
+    alertMsg.textContent = 'Name must be at least 2 characters.';
+    name.focus();
+  } else {
+    // input is valid
+    alertMsg.classList.remove('alert-msg--error');
+    alertMsg.classList.add('alert-msg--success');
+    alertMsg.textContent =
+      'Your message was sent successfully! I will contact you soon! Thanks :)';
+    sendEmail();
+  }
+});
+
+// onsubmit="sendEmail(); "
+
 function sendEmail() {
   var params = {
     name: document.getElementById('name').value,
@@ -38,7 +59,8 @@ function sendEmail() {
       document.getElementById('name').value = '';
       document.getElementById('email').value = '';
       document.getElementById('message').value = '';
-      alertMsg.classList.add('alert-msg--success');
     })
     .catch((err) => console.log(err));
+
+  return false;
 }
